6661cce7f1330f20514efbc577500b380d0fea95
Ahmad Beidas <BeidasA@exalt.ps>
2020-06-24 15:58:57 +0300

    Changing the order of packages3

--- src/main/java/org/bank/controller/MainController.java
+++ src/main/java/org/bank/controller/MainController.java
@@ -30,33 +30,6 @@ public class MainController {
     @Autowired
     CustomUserDetailsService userDetailsService;
 
-    @RequestMapping(path = "/transactions/deposit", method = RequestMethod.POST)
-    public ResponseEntity<String> deposit(@RequestHeader("Authorization") String authorizationHeader, @RequestBody TransactionRequestBody requestBody) throws ClassNotFoundException {
-        assertEqualZero(requestBody.getToUser());
-        float amount = requestBody.getAmount();
-        checkAmountIsPositive(amount);
-        int userId = transactionService.deposit(amount, authorizationHeader);
-        return new ResponseEntity<String>("Deposit to user " + userId + " of amount " + amount, HttpStatus.OK);
-    }
-
-    @RequestMapping(path = "/transactions/withdraw", method = RequestMethod.POST)
-    public ResponseEntity<String> withdraw(@RequestHeader("Authorization") String authorizationHeader, @RequestBody TransactionRequestBody requestBody) throws ClassNotFoundException {
-        assertEqualZero(requestBody.getToUser());
-        float amount = requestBody.getAmount();
-        checkAmountIsPositive(amount);
-        int userId = transactionService.withdraw(amount, authorizationHeader);
-        return new ResponseEntity<String>("Withdraw to user " + userId + " of amount " + amount, HttpStatus.OK);
-    }
-
-    @RequestMapping(path = "/transactions/transfer", method = RequestMethod.POST)
-    public ResponseEntity<String> transfer(@RequestHeader("Authorization") String authorizationHeader, @RequestBody TransactionRequestBody requestBody) throws ClassNotFoundException {
-        int transfereeId = requestBody.getToUser();
-        assertNotEqualZero(transfereeId);
-        float amount = requestBody.getAmount();
-        checkAmountIsPositive(amount);
-        int transfererId = transactionService.transfer(authorizationHeader, transfereeId, amount);
-        return new ResponseEntity<String>("Transfer from user with id " + transfererId + " to user with id " + transfereeId, HttpStatus.OK);
-    }
 
     @PostMapping(path = "/authenticate")
     public ResponseEntity<String> login(@RequestBody User user) throws Exception {
