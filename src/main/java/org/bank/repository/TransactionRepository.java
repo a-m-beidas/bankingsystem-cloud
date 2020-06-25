e9e46e80baf4472d0b26fdf0f62a9c9a221a70b9
Ahmad Beidas <BeidasA@exalt.ps>
2020-06-25 14:54:41 +0300

    See javadocs

--- src/main/java/org/bank/repository/TransactionRepository.java
+++ src/main/java/org/bank/repository/TransactionRepository.java
@@ -11,11 +11,25 @@ import java.util.Date;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
+    /**
+     * In order to add a transaction in hibernate one needs the complete persisted object as follows:
+     * {@code transactionRepository.save(new Transaction(persistedUser, ... otherFeilds)} <br>
+     * fetching the "persistedUser" to save the transaction will reduce performance, another alternative<br>
+     * would be to store the User object inside the CustomUserDetails Object but that will take up memory
+     * therefore native query was used
+     */
     @Modifying
     @Transactional
     @Query(nativeQuery = true, value="insert into transfer_transactions (by_user, to_user, transaction_type, amount, date) value (?,?,?,?,?)")
     void addTransferTransaction(int byUser, int toUser, String transactionType, float amount, Date date);
 
+    /**
+     * In order to add a transaction in hibernate one needs the complete persisted object as follows:
+     * {@code transactionRepository.save(new Transaction(persistedUser, ... otherFeilds)} <br>
+     * fetching the "persistedUser" to save the transaction will reduce performance, another alternative<br>
+     * would be to store the User object inside the CustomUserDetails Object but that will take up memory
+     * therefore native query was used
+     */
     @Modifying
     @Transactional
     @Query(nativeQuery = true, value="insert into non_transfer_transactions (by_user, transaction_type, amount, date) value (?,?,?,?)")
