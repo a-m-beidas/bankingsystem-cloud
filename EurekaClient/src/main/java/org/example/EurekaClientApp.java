09bbc60d9d1114e9705ae1fb629a59617f8f019f
Ahmad Beidas <BeidasA@exalt.ps>
2020-06-30 09:43:00 +0300

    .

---
+++ EurekaClient/src/main/java/org/example/EurekaClientApp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApp.class, args);
+    }
+}
