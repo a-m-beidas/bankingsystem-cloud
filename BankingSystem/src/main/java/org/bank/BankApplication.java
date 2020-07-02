4ac6bd28c8d918021e8b492a35527e5217de61b9
Ahmad Beidas <BeidasA@exalt.ps>
2020-07-02 10:59:18 +0300

    working gateway

--- BankingSystem/src/main/java/org/bank/BankApplication.java
+++ BankingSystem/src/main/java/org/bank/BankApplication.java
@@ -1,8 +1,10 @@
 package org.bank;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -14,5 +16,4 @@ public class BankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
-
 }
