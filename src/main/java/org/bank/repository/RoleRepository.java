b4b641dda2b0974e38a12d018041c38b40dbae66
Ahmad Beidas <BeidasA@exalt.ps>
2020-07-26 16:38:53 +0300

    .

--- src/main/java/org/bank/repository/RoleRepository.java
+++ src/main/java/org/bank/repository/RoleRepository.java
@@ -4,5 +4,4 @@ import org.bank.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-    JpaRepository jparepository = null;
 }
