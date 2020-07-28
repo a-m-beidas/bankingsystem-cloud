3effd9a50e1467f04cc92f2badbd9bb2b1438ba3
Ahmad Beidas <BeidasA@exalt.ps>
2020-07-28 16:03:03 +0300

    .

--- src/main/java/org/bank/config/DatabaseDummyData.java
+++ src/main/java/org/bank/config/DatabaseDummyData.java
@@ -39,6 +39,26 @@ public class DatabaseDummyData {
         users.add(new User("John", bcryptEncoder.encode("123"), 30000.0f, getRolesArrayList(user)));
         users.add(new User("Matt", bcryptEncoder.encode("124"), 500.0f, getRolesArrayList(user)));
         users.add(new User("Blake", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Candelaria", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Leonia", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Vanda", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Maya", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Tiera", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Neil", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Providencia", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Truman", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Pamila", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Keshia", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Hermelinda", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Milford", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Cristopher", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Kareem", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Guy", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Su", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Mariano", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Dayle", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Boyd", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
+        users.add(new User("Janeth", bcryptEncoder.encode("125"), 5050.f, getRolesArrayList(user)));
         userRepository.saveAll(users);
     }
 
