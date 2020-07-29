029a31535bfa04615fbe682d9cfbd529c8d482d7
Ahmad Beidas <BeidasA@exalt.ps>
2020-07-29 12:53:24 +0300

    .

--- src/main/java/org/bank/job/JobSchedule.java
+++ src/main/java/org/bank/job/JobSchedule.java
@@ -1,4 +1,14 @@
 package org.bank.job;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JobSchedule {
+
+
+    @Scheduled(fixedRate = 5000)
+    public void scheduled() {
+        System.out.println("Scheduled Job");
+    }
 }
