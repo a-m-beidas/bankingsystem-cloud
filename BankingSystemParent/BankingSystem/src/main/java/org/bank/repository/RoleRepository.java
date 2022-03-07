9cc6c4b1fdd7d571f141daba124b3356a8238325
A-beidas <A-beidas@github.com>
2022-03-07 15:01:00 +0200

    initial

--- BankingSystemParent/BankingSystem/src/main/java/org/bank/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package org.bank.repository;
-
-import org.bank.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    JpaRepository jparepository = null;
-}
